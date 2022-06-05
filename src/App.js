ad7977ce482ad9476171c4d4baef7b2d527772bc
neautish <75166589+neautish@users.noreply.github.com>
2022-06-05 22:10:54 +0430

    add expenses components to hold expense items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
-	const expense = [
+	const expenses = [
 		{
 			title: "Car Insurance",
 			amount: 250.25,
@@ -26,11 +26,7 @@ function App() {
 
 	return (
 		<div>
-			<h1>lets get started</h1>
-			<ExpenseItem title={expense[0].title} amount={expense[0].amount} date={expense[0].date} />
-			<ExpenseItem title={expense[1].title} amount={expense[1].amount} date={expense[1].date} />
-			<ExpenseItem title={expense[2].title} amount={expense[2].amount} date={expense[2].date} />
-			<ExpenseItem title={expense[3].title} amount={expense[3].amount} date={expense[3].date} />
+			<Expenses items={expenses} />
 		</div>
 	);
 }
